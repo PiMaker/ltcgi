be75bdf42b78923060a12dc75de0180fed522859
pi <stefan@pimaker.at>
2023-05-27 18:19:41 +0200

    followup, readd using for standalone and don't allow both buttons

--- Scripts/Editor/LTCGI_ControllerExternal.cs
+++ Scripts/Editor/LTCGI_ControllerExternal.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityEditor.Build.Reporting;
 #if VRC_SDK_VRCSDK2 || UDONSHARP
 using VRC.SDKBase.Editor.BuildPipeline;
 #endif
@@ -320,7 +321,7 @@ AudioLink: {(LTCGI_Controller.AudioLinkAvailable == LTCGI_Controller.AudioLinkAv
                 AssetDatabase.Refresh();
                 configChangedValues = new Dictionary<string, object>();
             }
-            if (revertButtonPressed)
+            else if (revertButtonPressed)
             {
                 configChangedValues = new Dictionary<string, object>();
             }
