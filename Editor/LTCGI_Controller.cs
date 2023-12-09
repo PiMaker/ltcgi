4c4c89a324414075388b384575593ce355299f9d
galister <22305755+galister@users.noreply.github.com>
2023-12-09 12:29:59 +0100

    use cross-platform paths for migration

--- Editor/LTCGI_Controller.cs
+++ Editor/LTCGI_Controller.cs
@@ -92,36 +92,20 @@ namespace pi.LTCGI
         public static void MigratoryBirdsDontMigrateAsMuchAsWeDoButThisFunctionWillTakeCareOfItNonetheless()
         {
             var hasChanges = false;
-            if (!AssetDatabase.IsValidFolder("Assets\\_pi_"))
+            var cgincPath = Path.Combine("Assets", "_pi_", "_LTCGI", "Shaders", "LTCGI.cginc");
+            if (!File.Exists(cgincPath))
             {
-                AssetDatabase.CreateFolder("Assets", "_pi_");
-                hasChanges = true;
-            }
-            if (!AssetDatabase.IsValidFolder("Assets\\_pi_\\_LTCGI"))
-            {
-                AssetDatabase.CreateFolder("Assets\\_pi_", "_LTCGI");
-                hasChanges = true;
-            }
-            if (!AssetDatabase.IsValidFolder("Assets\\_pi_\\_LTCGI\\Shaders"))
-            {
-                AssetDatabase.CreateFolder("Assets\\_pi_\\_LTCGI", "Shaders");
-                hasChanges = true;
-            }
-            if (!File.Exists("Assets\\_pi_\\_LTCGI\\Shaders\\LTCGI.cginc"))
-            {
-                File.WriteAllText("Assets\\_pi_\\_LTCGI\\Shaders\\LTCGI.cginc", "#include \"Packages\\at.pimaker.ltcgi\\Shaders\\LTCGI.cginc\"");
+                var contents = "#include \"" + Path.Combine("Packages", "at.pimaker.ltcgi", "Shaders", "LTCGI.cginc") + "\"";
+                Directory.CreateDirectory(Path.GetDirectoryName(cgincPath));
+                File.WriteAllText(cgincPath, contents);
                 hasChanges = true;
             }
 
-            // god I hate this part, Unity dumb dumb
-            if (!AssetDatabase.IsValidFolder("Assets\\Gizmos"))
+            var gizmoPath = Path.Combine("Assets", "Gizmos", "LTCGI_Screen_Gizmo.png"); 
+            if (!File.Exists(gizmoPath))
             {
-                AssetDatabase.CreateFolder("Assets", "Gizmos");
-                hasChanges = true;
-            }
-            if (!File.Exists("Assets\\Gizmos\\LTCGI_Screen_Gizmo.png"))
-            {
-                File.Copy("Packages\\at.pimaker.ltcgi\\LTCGI_Screen_Gizmo.png", "Assets\\Gizmos\\LTCGI_Screen_Gizmo.png", true);
+                Directory.CreateDirectory(Path.GetDirectoryName(gizmoPath));
+                File.Copy(Path.Combine("Packages", "at.pimaker.ltcgi", "LTCGI_Screen_Gizmo.png"), gizmoPath, true);
                 hasChanges = true;
             }
 
