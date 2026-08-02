b2014d6c6e76c551c30084973e54687941265d68
pi <pi@pimaker.at>
2026-08-02 18:35:48 +0200

    bump version to 1.7.3

--- Editor/LTCGI_ControllerExternal.cs
+++ Editor/LTCGI_ControllerExternal.cs
@@ -17,7 +17,7 @@ namespace pi.LTCGI
     [CustomEditor(typeof(LTCGI_Controller))]
     public class LTCGI_ControllerEditor : Editor
     {
-        public const string VERSION = "v1.7.2";
+        public const string VERSION = "v1.7.3";
 
         private static readonly string[] CONFIGURATION_PROPS = new[] {
             #if UNITY_STANDALONE
